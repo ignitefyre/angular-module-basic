68aeafc153eafaaeef59f5d30fbaf08aeef8d19d
Corey Prater <cprater02@gmail.com>
2021-01-03 15:23:43 -0600

    build updates

--- projects/basic-demo/src/lib/basic-demo.module.ts
+++ projects/basic-demo/src/lib/basic-demo.module.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BasicDemoComponent } from './basic-demo.component';
-
-
 
 @NgModule({
   declarations: [BasicDemoComponent],
   imports: [
+    CommonModule
   ],
   exports: [BasicDemoComponent]
 })
--- projects/basic-demo/src/public-api.ts
+++ projects/basic-demo/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of basic-demo
  */
 
-export * from './lib/basic-demo.service';
-export * from './lib/basic-demo.component';
 export * from './lib/basic-demo.module';
